e922ff1abf7100452723be06045b66c11326974f
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-07-11 18:39:42 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ class App < Sinatra::Base
   end
 
   get "/multiply/:num1/:num2" do
-    <% @solution = params[:num1] * params[:num2] %>
-    <%= "#{@solution}" %>
+    @solution = params[:num1] * params[:num2]
+    "#{@solution}"
   end
 end
